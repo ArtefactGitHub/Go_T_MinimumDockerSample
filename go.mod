1e2f507666c096b2a409d57e7adae3ef375e74ac
ArtefactGitHub <artefactgo@gmail.com>
2022-03-08 16:42:34 +0900

    main.goなど最小構成を追加

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ArtefactGitHub/Go_T_MinimumDockerSample
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Hello world")
+
+	router := http.NewServeMux()
+	router.HandleFunc("/", Home)
+
+	http.ListenAndServe(":8080", router)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("home")
+}
